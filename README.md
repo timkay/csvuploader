60bee19efdf6d229bc579c42215c643e4d22c7d7
timkay <timkay@not.com>
2021-04-01 12:14:43 -0700

    added documentation

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # csvuploader
-Upload a CSV to an in-memory database
+
+Upload a CSV to an in-memory database.
+
+## Scenario
+
+> You work on a product that receives potentially large files in CSV format, process them and import their data into our database. However, providers do not all use the same layout of columns. The order may differ between providers, they may only send a subset of the columns, or they may include additional columns we do not wish to capture.
+
+The "potentially large" modifier indicates that the implementation should avoid loading the entire CSV file into memory for processing. Instead, the code streams the upload throught the database insertion process.
+
+## Task
+> Build an API with a single endpoint that accepts a file upload in the CSV format and the provider name that sent the file, looks up a configuration defining the column layout and parses the CSV into an in-memory database (e.g. SQLite, mongodb-memory-server). The columns we care about are defined below in the “Columns” section.
+
+The requirements are somewhat confusing. CSV files usually have a first row that contains the column headers. In that case, the confuration file would be unnecessary. Therefore, we make the assumption that the CSV files will contain no column headers.
+
+### Framework / Libraries / Architecture
+
+The server is implemented in nodejs using the express framework. Additional packages yaml, connect-busboy (facilitates streaming of the uploaded CSV file), csv-parser (provides CSV parsing with streaming), and sqlite3 provide the needed functionality to meet the requirements.
+
+### Configuration File
+
+The configuration file uses YAML format because it is simple and easy to maintain. The format is obvious, so no additional description is provided.
+
+A default configuration is included, which will be used in case the user specifies a provider that does not exist.
+
+### In-Memory Database
+
+The data is stored in sqlite3, chosen because it supports both in-memory and persistent capabilities. For testing, the data is written to the filesystem and can be viewed using the sqlite3 command line.
+
+### API
+
+The API is a single endpoint with a single method: /api/v1/upload. A POST request should contain MIME-encoded data with two fields: The `provider` and `file` (although the name of the field for the uploaded file is not used. The API request returns a JSON object containing the provider, provider name (from the configuration file), the column names, and the number of rows processed.
+
+For debugging, an addtional API method provides a dump of the rows for a given provider: /api/v1/dump. See public/dump.html for a usage example.
+
+The code is running at
+
+### To Do
+
+A lot of work needs to be done to productize the code, such as checking that the specified provider exists. Of course, a more likely scenario is that a user would sign in, and their provider would automatically be specified, so the error handling would be different.
+
+
---
+++ app.js
@@ -0,0 +1,93 @@
+
+const fs = require('fs');
+const yaml = require('yaml');
+const express = require('express');
+const busboy = require('connect-busboy');
+const csv = require('csv-parser');
+const sqlite3 = require('sqlite3');
+
+const settings = yaml.parse(fs.readFileSync('settings.yaml', 'utf8'));
+// sanity check settings: make sure column names do not contain apostrophe
+
+let dbfile = ':memory:';
+
+// Persists the data to the filesystem for debugging.
+if (true) {
+    // make sure the data subdirectory exists
+    try {fs.mkdirSync('data')} catch (e) {};
+    dbfile = 'data/sqlite.db';
+}
+
+const db = new sqlite3.Database('data/sqlite.db');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(busboy());
+
+app.post('/api/v1/upload', (req, res) =>  {
+
+    let provider = 'default';
+
+    // busboy is used to parse the uploaded file because it streams. This way
+    // the upload can be processed in flight rather than first loading the whole
+    // thing into memory. Allows for much larger file uploads.
+
+    req.busboy.on('field', (key, value, keyTruncated, valueTruncated) => {
+        //console.log(key, value, keyTruncated, valueTruncated);
+        if (key === 'provider') provider = value;
+    });
+
+    req.busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        //console.log('file', fieldname, filename, encoding, mimetype);
+
+        // The provider value is normalized: only lower case word characters are allowed
+        // Then look up the columns for this provider.
+        provider = provider.replace(/\W+/g, '').toLowerCase();
+        const {columns: selection} = settings.providers.default;
+        const {name, columns} = settings.providers[provider] || settings.providers.default;
+        const dbname = `data/${provider}.db`;
+
+        db.serialize(() => {
+            db.run(`drop table if exists ${provider}`, [], () => {
+
+                const cmd1 = `create table ${provider} (${columns.map(col => `'${col}' text`).join(', ')})`;
+                db.run(cmd1, [], () => {
+
+                    // The insert command happens a lot, but it's always the same, so we build it here.
+                    // The values for each row will be inserted using placeholders.
+                    const cmd2 = `insert into ${provider} values (${columns.map(() => '?').join(', ')})`;
+
+                    let n = 0;
+
+                    file
+                        .pipe(csv(columns))
+                        .on('data', async data => {
+                            n += 1;
+                            const values = selection.map(item => data[item] || '');
+                            db.run(cmd2, values);
+                        })
+                        .on('end', () => {
+                            res.json({status: 'success', provider, name, n});
+                        });
+                });
+            });
+        });
+    });
+
+    req.pipe(req.busboy);
+});
+
+app.get('/api/v1/dump',  (req, res) => {
+    const provider = req.query.provider || 'default';
+    const {name, columns} = settings.providers.default;
+    db.all(`select * from ${provider}`, (error, rows) => {
+        if (error) console.log('error', error);
+        if (rows) res.json({provider, name, columns, rows: rows.map(row => Object.values(row))});
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/`);
+});
