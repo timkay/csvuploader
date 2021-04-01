5304b1bcecaa20083b5fc21442ebbfca3b0ebae8
timkay <timkay@not.com>
2021-04-01 01:09:20 -0700

    snapshot

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 
+const fs = require('fs');
+const yaml = require('yaml');
 const express = require('express');
 const busboy = require('connect-busboy');
-const fs = require('fs');
 const csv = require('csv-parser');
+const sqlite3 = require('sqlite3');
+const {open} = require('sqlite');
+
+const settings = yaml.parse(fs.readFileSync('settings.yaml', 'utf8'));
+// sanity check settings: make sure column names do not contain apostrophe
 
 const app = express();
 const port = 3000;
@@ -12,6 +18,7 @@ app.use(busboy());
 
 app.post('/api/v1/upload', (req, res) =>  {
 
+    let provider = 'default';
     let n = 0;
 
     // busboy is used to parse the uploaded file because it streams. This way
@@ -19,21 +26,51 @@ app.post('/api/v1/upload', (req, res) =>  {
     // thing into memory. Allows for much larger file uploads.
 
     req.busboy.on('field', function (key, value, keyTruncated, valueTruncated) {
-        console.log(key, value, keyTruncated, valueTruncated);
+        //console.log(key, value, keyTruncated, valueTruncated);
+        if (key === 'provider') provider = value;
     });
 
     req.busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
-        console.log('file', fieldname, filename, encoding, mimetype);
-        file
-        .pipe(csv())
-        .on('data', data => {
-            n += 1;
-            console.log('data', data);
-        })
-        .on('end', () => {
-            console.log('end');
-            res.end(`Uploaded ${n} lines`);
-        });
+        //console.log('file', fieldname, filename, encoding, mimetype);
+
+        (async function () {
+
+            // The provider value is normalized: only lower case word characters are allowed
+            // Then look up the columns for this provider.
+            provider = provider.replace(/\W+/g, '').toLowerCase();
+            const {name, columns} = settings.providers[provider] || settings.providers.default;
+            const dbname = `data/${provider}.db`;
+
+            // make sure the data subdirectory exists
+            try {await fs.promises.mkdir('data')} catch (e) {console.log(e)};
+            try {await fs.promises.unlink(dbname)} catch (e) {};
+            const db = await open({filename: dbname, driver: sqlite3.Database});
+
+            const cmd1 = `create table data (${columns.map(col => `'${col}' text`).join(', ')})`;
+            console.log(cmd1);
+            await db.exec(cmd1);
+
+            // The insert command happens a lot, but it's always the same, so we build it here.
+            // The values for each row will be inserted using placeholders.
+            const cmd2 = `insert into data values (${columns.map(col => '?').join(', ')})`;
+
+            console.log(cmd2);
+
+            file
+            .pipe(csv())
+            .on('data', async data => {
+                n += 1;
+                //console.log('data', data);
+                const values = columns.map(item => data[item] || '');
+                console.log(values);
+                await db.exec(cmd2, values);
+            })
+            .on('end', () => {
+                console.log('end');
+                res.end(`Uploaded ${n} lines for provider ${provider}=${name}`);
+            });
+
+        })();
     });
 
     req.pipe(req.busboy);
